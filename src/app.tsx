a833e12130e4b41d0d1814a17535673dc1f61071
Daiana de Paula <daianaadepaula1@gmail.com>
2024-02-22 12:36:39 -0300

    feat: add server json

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,40 @@ import { Tabs } from './components/tabs'
 import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
+import { useQuery } from '@tanstack/react-query'
+
+export interface tagsResponse {
+	first: number
+	prev: number | null
+	next: number
+	last: number
+	pages: number
+	items: number
+	data: Tag[]
+}
+
+export interface Tag {
+	title: string
+	amountOfVideos: number
+	id: string
+}
 
 export function App() {
+	const { data: tagsResponse, isLoading } = useQuery<tagsResponse>({
+		queryKey: ['get-tags'],
+		queryFn: async () => {
+			const response = await fetch('http://localhost:3333/tags?_page1&_per_page=10')
+			const data = await response.json()
+			console.log(data);
+
+
+			return data
+		}
+	})
+
+	if (isLoading) {
+		return null
+	}
 
 	return (
 		<div className="py-10 space-y-8">
@@ -46,18 +78,18 @@ export function App() {
 					</TableHeader>
 
 					<TableBody>
-						{Array.from({ length: 10 }).map((value, index) => {
+						{tagsResponse?.data.map((tag) => {
 							return (
-								<TableRow key={index}>
+								<TableRow key={tag.id}>
 									<TableCell></TableCell>
 									<TableCell>
 										<div className='flex flex-col gap-0.5'>
-											<span className='font-medium'>React</span>
-											<span className='text-xs text-zinco-500'>T5P-HJ5U685HNH-5U8568GB5</span>
+											<span className='font-medium'>{tag.title}</span>
+											<span className='text-xs text-zinco-500'>{tag.id}</span>
 										</div>
 									</TableCell>
 									<TableCell className='text-zinco-300'>
-										13 video(s)
+										{tag.amountOfVideos} video(s)
 									</TableCell>
 									<TableCell className='text-right'>
 										<Button className='icon'>
