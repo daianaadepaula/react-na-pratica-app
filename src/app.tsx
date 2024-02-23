0e96422c0e6dd46f211cf85bd9f69bcbeb2bffde
Daiana de Paula <daianaadepaula1@gmail.com>
2024-02-23 10:39:07 -0300

    feat: searchParams and routerProvider

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,8 @@ import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
 import { useQuery } from '@tanstack/react-query'
+import { Pagination } from './components/pagination'
+import { useSearchParams } from 'react-router-dom'
 
 export interface tagsResponse {
 	first: number
@@ -23,10 +25,14 @@ export interface Tag {
 }
 
 export function App() {
+	const [searchParams] = useSearchParams()
+
+	const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
+
 	const { data: tagsResponse, isLoading } = useQuery<tagsResponse>({
 		queryKey: ['get-tags'],
 		queryFn: async () => {
-			const response = await fetch('http://localhost:3333/tags?_page1&_per_page=10')
+			const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10`)
 			const data = await response.json()
 			console.log(data);
 
@@ -83,17 +89,17 @@ export function App() {
 								<TableRow key={tag.id}>
 									<TableCell></TableCell>
 									<TableCell>
-										<div className='flex flex-col gap-0.5'>
-											<span className='font-medium'>{tag.title}</span>
-											<span className='text-xs text-zinco-500'>{tag.id}</span>
+										<div className="flex flex-col gap-0.5">
+											<span className="font-medium">{tag.title}</span>
+											{/* <span className="text-xs text-zinc-500">{tag.slug}</span> */}
 										</div>
 									</TableCell>
-									<TableCell className='text-zinco-300'>
+									<TableCell className="text-zinc-300">
 										{tag.amountOfVideos} video(s)
 									</TableCell>
-									<TableCell className='text-right'>
-										<Button className='icon'>
-											<LucideMoreHorizontal className='size-4' />
+									<TableCell className="text-right">
+										<Button size="icon">
+											<LucideMoreHorizontal className="size-4" />
 										</Button>
 									</TableCell>
 								</TableRow>
@@ -102,8 +108,8 @@ export function App() {
 					</TableBody>
 				</Table>
 
+				{tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={page} />}
 			</main>
-
 		</div>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client'
 import { App } from './app'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<App />
+			<RouterProvider router={router} />
 		</QueryClientProvider>
 	</React.StrictMode>,
 )
